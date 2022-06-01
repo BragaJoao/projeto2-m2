2d98cdb019857ee986bc329339bbd52e66b9fc84
BragaJoao <joaogbraga@hotmal.com.br>
2022-05-31 22:57:49 -0300

    Fourth commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
+const port = process.env.PORT || 3000;
+
 const pokedex = [
   {
     id: 1,
@@ -92,6 +94,6 @@ app.get("/delete/:id", (req, res) => {
   res.redirect("/#cards");
 });
 
-app.listen(3000, () =>
+app.listen(port, () =>
   console.log("Servidor rodando em http://localhost:3000")
 );
